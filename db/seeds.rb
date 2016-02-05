f3d882e2fe944010ba8d478020b5413134e0dc9a
Lyubomr <s.lyubomyr@gmail.com>
2016-02-05 14:11:57 -0500

    Updated show page for reservations model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-Restaurant.create!(:name => "McDonalds", :cuisine => "Crap")
-User.create!(:first_name => "Crotch", :last_name => "Rocket", :email => "CrotchR@gmail.com")
+Restaurant.create!(:name => "McDonalds", :cuisine => "Crap", :address => "220 King Street West", :price_range => "Dirt Shit Cheap", :phone => "647 123 4567", :capacity => 100)
+Restaurant.create!(:name => "Soup Nutsy", :cuisine => "Witch Stews", :address => "220 King Street West", :price_range => "A little pricey", :phone => "647 123 2222", :capacity => 50)
+Restaurant.create!(:name => "The Keg", :cuisine => "Steak & Shit", :address => "CN Tower street", :price_range => "Better put out", :phone => "647 666 9999", :capacity => 300)
+User.create!(:first_name => "Crotch", :last_name => "Rocket", :email => "CrotchR@gmail.com", :password_digest => "aaaa")
