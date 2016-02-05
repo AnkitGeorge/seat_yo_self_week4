f46dc9652fa48a723027ef3f34a3aee725122b52
Lyubomr <s.lyubomyr@gmail.com>
2016-02-05 14:21:05 -0500

    Configured root to log in page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -29,7 +29,7 @@ class ReservationsController < ApplicationController
   def create
     @reservation = Reservation.new(reservation_params)
     if @reservation.save
-      redirect_to restaurant_reservation_path(@reservation)
+      redirect_to restaurant_reservations_path(@reservation)
     else
       render :new
     end
